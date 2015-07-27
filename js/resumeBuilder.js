940fa06e2cbcbe973892dfbcbdbca2942f7d5bc1
Graham Lutz <graham.lutz@gmail.com>
2015-07-27 10:55:13 -0400

    Add my name to #main

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ $("#main").append("Graham lutz");
+
+ $("#education").append("HTML, CSS, JavaScript, jQuery, Git, GitHub, Command Line, Grunt, Bootstrap, Node.js");
+ 
